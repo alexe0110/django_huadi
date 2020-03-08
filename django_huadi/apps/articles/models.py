5e3c5936052ca178780fe719a7a0ab47293e3177
alexe0110 <alexe0110@yandex.ru>
2020-03-08 17:22:25 +0500

    add view and models

--- django_huadi/apps/articles/models.py
+++ django_huadi/apps/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    article_title = models.CharField('Название статьи', max_length=200)
+    article_text = models.TextField('Тест статьи')
+    pub_date = models.DateTimeField('Дата публикации')
+
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)  # Внешний ключ, для связи между моделями
+    author_name = models.CharField('Имя автора', max_length=100)
+    comment_text = models.CharField('Текст комментария', max_length=300)
--- django_huadi/apps/articles/views.py
+++ django_huadi/apps/articles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Привет МИР")
+
+
+def test(request):
+    return HttpResponse("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Тест</title>
+        </head>
+        <body>
+            <h1>Это тетовая старница</h1>
+            <h3>Проверка</h3>
+        </body>
+        </html>
+    """)
